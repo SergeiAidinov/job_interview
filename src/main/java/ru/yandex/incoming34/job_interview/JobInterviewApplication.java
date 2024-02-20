d34ea1a21b9ace3e9d33c55e57c6df324b6f4d99
SergeiAidinov <incoming34@yandex.ru>
2024-02-20 16:01:15 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/incoming34/job_interview/JobInterviewApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.incoming34.job_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/incoming34/job_interview/JobInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.incoming34.job_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
