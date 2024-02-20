a41b3a2af459c8f36a6804bd9c83ba9cdc1a15de
SergeiAidinov <incoming34@yandex.ru>
2024-02-20 16:15:46 +0300

    Сконфигурировал Swagger

--- src/main/java/ru/yandex/incoming34/job_interview/JobInterviewApplication.java
+++ src/main/java/ru/yandex/incoming34/job_interview/JobInterviewApplication.java
@@ -3,10 +3,13 @@ package ru.yandex.incoming34.job_interview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JobInterviewApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(JobInterviewApplication.class, args);
 	}
 
