56766b46fc8496578372ce6cef47ccb33eb123fb
SergeiAidinov <incoming34@yandex.ru>
2024-02-20 20:09:11 +0300

    Сделал метод allClients() в контроллере

--- src/main/java/ru/yandex/incoming34/job_interview/controller/Controller.java
+++ src/main/java/ru/yandex/incoming34/job_interview/controller/Controller.java
@@ -1,11 +1,17 @@
 package ru.yandex.incoming34.job_interview.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.incoming34.job_interview.entity.Client;
 import ru.yandex.incoming34.job_interview.repo.ClientRepo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -17,5 +23,10 @@ public class Controller {
     @PostMapping(value = "/new_client")
     public void addClient(String clientName){
         clientRepo.save(new Client(clientName));
+    }
+
+    @GetMapping(value = "/all_clients")
+    public Iterable<Client> allClients() {
+        return clientRepo.findAll();
     }
 }
--- src/main/java/ru/yandex/incoming34/job_interview/entity/Client.java
+++ src/main/java/ru/yandex/incoming34/job_interview/entity/Client.java
@@ -1,9 +1,15 @@
 package ru.yandex.incoming34.job_interview.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name ="clients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Client {
 
     @Id
@@ -12,7 +18,7 @@ public class Client {
     private Long id;
 
     @Column(name = "client_name")
-    private final String name;
+    private String name;
 
     public Client(String name) {
         this.name = name;
