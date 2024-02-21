428903e8d39beec324d384e9cb62b29141eb58de
SergeiAidinov <incoming34@yandex.ru>
2024-02-21 14:58:22 +0300

    Сделал логирование

--- src/main/java/ru/yandex/incoming34/job_interview/structures/entity/email/AbstractClientEmail.java
+++ src/main/java/ru/yandex/incoming34/job_interview/structures/entity/email/AbstractClientEmail.java
@@ -2,19 +2,21 @@ package ru.yandex.incoming34.job_interview.structures.entity.email;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import ru.yandex.incoming34.job_interview.structures.entity.AbstractContact;
 
 @MappedSuperclass
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public abstract class AbstractClientEmail extends AbstractContact {
 
     @Id
     private String email;
 
-    public AbstractClientEmail(String email) {
+    /*public AbstractClientEmail(String email) {
         this.email = email;
-    }
+    }*/
 }
